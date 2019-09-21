654ff90ea931d30429c1aa6cb919d2f95fc1578d
Ivan Danci <bah4uk@gmail.com>
2019-09-21 16:53:02 +0300

    Implement users create

--- api.rb
+++ api.rb
@@ -7,3 +7,22 @@ get '/' do
 
   DB[:users].order(Sequel.desc(order_column)).all.to_json
 end
+
+post '/' do
+  content_type :json
+
+  request.body.rewind
+
+  data = JSON.parse request.body.read
+  user_attributes = data.dig('data', 'attributes') || {}
+
+  user = User.new(user_attributes)
+
+  if user.valid?
+    status 201
+    user.save.to_json
+  else
+    status 406
+    user.errors.to_json
+  end
+end
